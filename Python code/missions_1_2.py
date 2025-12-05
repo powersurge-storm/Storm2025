a6bef559b9fd9b1c03487917c1f52a280ae7f6df
Powersurge Storm <powersurge.storm@gmail.com>
2025-12-04 18:04:15 -0600

    Create missions_1_2.py
    Code for missions 1 & 2

---
+++ Python code/missions_1_2.py
@@ -0,0 +1,63 @@
+from hub import port
+import motor_pair
+from hub import motion_sensor
+import motor
+import runloop
+
+async def main():
+   motion_sensor.set_yaw_face(motion_sensor.FRONT)
+   motion_sensor.reset_yaw(0)
+   motor_pair.unpair(motor_pair.PAIR_1)
+   motor_pair.pair(motor_pair.PAIR_1,port.B,port.D)
+
+   motor_pair.move(motor_pair.PAIR_1,0,velocity=847)
+   await runloop.sleep_ms(1770)
+   motor_pair.stop(motor_pair.PAIR_1)
+   motion_sensor.reset_yaw(0)# reset yaw angle  
+   await runloop.sleep_ms(1770)
+
+   await motor.run_for_degrees(port.D,200, 300)
+   motor.stop(port.D)
+
+   motor_pair.move(motor_pair.PAIR_1,0,velocity=180)
+   await runloop.sleep_ms(1770)
+   motor_pair.stop(motor_pair.PAIR_1)
+   motion_sensor.reset_yaw(0)# reset yaw angle
+
+   #Lift right atatchment up
+
+   await motor.run_for_degrees(port.F,-270,300)
+   motor.stop(port.F)
+
+   motor_pair.move(motor_pair.PAIR_1,0,velocity=-180)
+   await runloop.sleep_ms(1270)
+   motor_pair.stop(motor_pair.PAIR_1)
+   motion_sensor.reset_yaw(0)# reset yaw angle
+
+   #Turn back toward base using right wheel
+   await motor.run_for_degrees(port.D,-230,300)
+   motor.stop(port.D)
+
+   #Move back to align with rubberband attachment
+   await motor_pair.move_for_degrees(motor_pair.PAIR_1,-290, 0)
+
+   await motor.run_for_degrees(port.C,-400,500)
+   motor.stop(port.C)
+
+   await motor.run_for_degrees(port.C, 400,500)
+   motor.stop(port.C)
+
+   motor_pair.move(motor_pair.PAIR_1,0,velocity=-800)
+   await runloop.sleep_ms(1770)
+   motor_pair.stop(motor_pair.PAIR_1)
+   motion_sensor.reset_yaw(0)# reset yaw angle
+  
+#    motion_sensor.reset_yaw(0)# reset yaw angle
+#    while motion_sensor.tilt_angles()[0]<900:#getting yaw value from tuple
+#        motor_pair.move(motor_pair.PAIR_1,-10)#move to left
+#    motor_pair.stop(motor_pair.PAIR_1)#stop motor
+    
+#    await motor.run_for_time(port.C,1000,-300)
+#    motor.stop(port.C)
+
+runloop.run(main())
