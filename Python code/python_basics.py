834e0e23e070078403cd105b2a0da0ae1e05a19c
Powersurge Storm <powersurge.storm@gmail.com>
2026-01-16 19:10:01 -0600

    Implement gyro-based movement functions
    Added gyro_forward and gyro_backward functions for motor control.

--- Python code/python_basics.py
+++ Python code/python_basics.py
@@ -11,21 +11,44 @@ import motor
 import color_sensor
 import color
 
+LEFT = port.B
+RIGHT = port.D
+LEFT_ATTACHMENT = port.C
+RIGHT_ATTACHMENT = port.F
+
+async def gyro_forward(distance, speed):
+    motion_sensor.reset_yaw(0)# reset yaw angle
+    motor.reset_relative_position(LEFT, 0)
+    steer = 0
+    while(motor.relative_position(LEFT)>=(-1 * distance)):
+        error = motion_sensor.tilt_angles()[0] * -0.1
+        correction = int(error * -2)
+        motor_pair.move(motor_pair.PAIR_1, correction, velocity=speed)
+    motor_pair.stop(motor_pair.PAIR_1,stop=motor.BRAKE)
+
+async def gyro_backward(distance, speed):
+    motion_sensor.reset_yaw(0)# reset yaw angle
+    motor.reset_relative_position(LEFT, 0)
+    steer = 0
+    print(motor.relative_position(LEFT))
+    while(motor.relative_position(LEFT)<=distance):
+        error = motion_sensor.tilt_angles()[0] * -0.1
+        # correction is an integer which is the negative of the error
+        correction = int(error * 2)
+        motor_pair.move(motor_pair.PAIR_1, correction, velocity= (-1*speed))
+    motor_pair.stop(motor_pair.PAIR_1,stop=motor.BRAKE)
 
 async def main():
-  # write your code here
-    #Setting initial Yaw angle on motion_sensor
     motion_sensor.set_yaw_face(motion_sensor.FRONT)
     motion_sensor.reset_yaw(0)# reset yaw angle
-   
     motor_pair.unpair(motor_pair.PAIR_1) # unpairs the motors in case it conflicts with previous code
-    motor_pair.pair(motor_pair.PAIR_1,port.B,port.D) #creating new pair motors at
+    motor_pair.pair(motor_pair.PAIR_1,LEFT,RIGHT) #creating new pair motors at        this whole chunk from 9 to 14 is to activate the driving motors and reseting the bot to move smoothly
+    
+    # Move forward to be close to the Map reveal
+    await gyro_forward(distance=1430,speed=500)
 
     # Move driving motors for 1300 degrees
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, 1300, 0, velocity=600, acceleration= 100, deceleration=100)
-
-    #Use Attatchment
-    await motor.run_for_degrees(port.C, 200,200)
 
     #Turn Right
     motion_sensor.reset_yaw(0)# reset yaw angle
@@ -39,6 +62,10 @@ async def main():
 
     # Turn for 150 degrees after color sensor detects white
     await motor.run_for_degrees(port.D,150,150)
+
+    
+    #Use Attatchment
+    await motor.run_for_degrees(port.C, 200,200)
 
     #Turn Left
     while motion_sensor.tilt_angles()[0]<900:#getting yaw value from tuple
