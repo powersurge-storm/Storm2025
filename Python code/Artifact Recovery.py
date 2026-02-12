3ca5cf47d34dae39a61e45c0ef5823a88e3b8293
Powersurge Storm <powersurge.storm@gmail.com>
2026-02-12 10:29:45 -0600

    Refactor robot movement functions and add comments
    Refactor robot movement code to improve clarity and functionality. Added comments for better understanding and adjusted motor control logic.

--- Python code/Artifact Recovery.py
+++ Python code/Artifact Recovery.py
@@ -1,14 +1,28 @@
+'''
+Author(s): Darshan Ayyanar, Vihaan Cheepurupalli, Shrihan Vemula
+Team Powersurge Storm 2025
+This code solves 3 missions: Careful Recovery, Statue Rebuild & Mineshaft delivery
+'''
 import runloop
 from hub import port
 import motor_pair
 from hub import motion_sensor
 import motor
+import color_sensor
+import color
 
-LEFT = port.B
-RIGHT = port.D
-LEFT_ATTACHMENT = port.C
+# Defining Port variables for easy access
+LEFT = port.B # Left Drive wheel
+RIGHT = port.D # Right Drive wheel
+LEFT_ATTACHMENT = port.C 
 RIGHT_ATTACHMENT = port.F
 
+'''
+Drive straight code using yaw angles to auto correct Robot's movement while driving forward
+Inputs: 
+  distance: Distance RObot has to Travel
+  speed: velocity at which Robot has to travel
+'''
 async def gyro_forward(distance, speed):
     motor.reset_relative_position(LEFT, 0)
     motion_sensor.reset_yaw(0)
@@ -21,11 +35,17 @@ async def gyro_forward(distance, speed):
         motor_pair.move(motor_pair.PAIR_1, correction, velocity=speed)
     motor_pair.stop(motor_pair.PAIR_1,stop=motor.BRAKE)
 
+'''
+Drive straight code using yaw angles to auto correct Robot's movement while driving back
+Inputs:
+distance: Distance Robot has to Travel
+speed: velocity at which Robot has to travel
+'''
 async def gyro_backward(distance, speed):
     motion_sensor.reset_yaw(0)
     motor.reset_relative_position(LEFT, 0)
     steer = 0
-    print(motor.relative_position(LEFT))
+    #print(motor.relative_position(LEFT))
     while(motor.relative_position(LEFT)<=distance):
         #print(motor.relative_position(LEFT))
         error = motion_sensor.tilt_angles()[0] * -0.1
@@ -36,90 +56,58 @@ async def gyro_backward(distance, speed):
         motor_pair.move(motor_pair.PAIR_1, correction, velocity= (-1*speed))
     motor_pair.stop(motor_pair.PAIR_1,stop=motor.BRAKE)
 
+# main function to write code to move Robot and attachments
 async def main():
+    #Resetting Robot Yaw face and yaw angles before it makes any movements
     motion_sensor.set_yaw_face(motion_sensor.FRONT)
     motion_sensor.reset_yaw(0)# reset yaw angle
     motor_pair.unpair(motor_pair.PAIR_1) # unpairs the motors in case it conflicts with previous code
     motor_pair.pair(motor_pair.PAIR_1,LEFT,RIGHT) #creating new pair motors at        this whole chunk from 9 to 14 is to activate the driving motors and reseting the bot to move smoothly
-
+    color_sensor.reflection(port.A)
     # *****Code for this run starts here *****
-    await motor.run_for_degrees(port.C,300,600)
-    motor.run_for_degrees(port.F,500,600 )
 
-    # Move forward to hit the wall
+    # Before we start moving, lift both attachments up
+    motor.run_for_degrees(LEFT_ATTACHMENT,300,600)
+    motor.run_for_degrees(RIGHT_ATTACHMENT,500,600 )
+
+    # Move forward all the way to hit the wall
     await gyro_forward(distance=1900,speed=800)
-     
-     #move back from wall
-    await gyro_backward(distance=125, speed=100)    
+
+     #move back from wall to get turning room
+    await gyro_backward(distance=100, speed=100)    
     
-    #Make a right turn to align with Mineshaft using left wheel
-    await motor.run_for_degrees(LEFT, 440, -350)
+    # print(motion_sensor.tilt_angles()[0])
 
-    await gyro_backward(distance=75, speed=50)
+    #Make a right turn to align with Mineshaft using left wheel while reading tilt angles, 
+    # and stop after it reaches desired yaw angle
+    while(motion_sensor.tilt_angles()[0]>-850):
+        motor.run(LEFT,-200)
+    motor.stop(LEFT)
 
-    await motor.run_for_degrees(port.C, 300,-600)
-    motor.run_for_degrees(port.F, 400,-600)
+    # print(motion_sensor.tilt_angles()[0])
 
+    # Move slightly back to get enough room to drop attachments down
+    await gyro_backward(distance=75, speed=200)
+
+    #Drop attachments 
+    await motor.run_for_degrees(LEFT_ATTACHMENT, 300,-600)
+    await motor.run_for_degrees(RIGHT_ATTACHMENT, 450,-600)
+
+    #Move forward until Black color is detected
+    while color_sensor.color(port.A) is not color.BLACK:
+        motor_pair.move(motor_pair.PAIR_1, 0, velocity=90)
+        #if color_sensor.color(port.A) is color.BLACK:
+        #    print("black")
+    motor_pair.stop(motor_pair.PAIR_1)
+
+    # Lift Right attachment to send Mineshaft to other side of the table
+    await motor.run_for_degrees(RIGHT_ATTACHMENT, 450, 500)
+
+    # Lift Left attachment to lift precious artifact with low velocity
+
+    await motor.run_for_degrees(LEFT_ATTACHMENT, 60, 100)
     
-    await gyro_forward(distance=315,speed=125)
-    #lift artifact and send mineshaft
-    await motor.run_for_degrees(port.C, 70, 475)
-    motor.run_for_degrees(port.F, 500, 950)
-    
-    
-    await gyro_backward(distance=150,speed=220)
-
-
-    await motor.run_for_degrees(port.F,300,600)
-
-    await motor.run_for_degrees(port.C,40,200)
-
-
-    await gyro_backward(distance=250,speed=200)
-
-    await motor.run_for_degrees(port.C,150,400)
-
-    await gyro_forward(distance=100,speed=400)
-
-    #Turn slightly towards base to give turning room
-    await motor.run_for_degrees(LEFT, 200, 400)
-
-    #Turn slightly towards Seal
-    await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1,250, 400,-400)
-
-    #Move to Seal
-    await gyro_forward(distance=600,speed=600)
-
-    #Attachment down for Seal
-    await motor.run_for_degrees(port.F,350,-500)
-
-    #Sneak under Seal
-    await gyro_forward(distance=300,speed=300)
-
-    #Lift for Seal
-    await motor.run_for_degrees(port.F,120, 600)
-
-    #Turn slightly Seal
-    await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1,150, -300,300)
-
-    #Lift for Seal
-    await motor.run_for_degrees(port.F,200, 600)
-
-    #Move to away from seal
-    await gyro_backward(distance=100,speed=400)
-
-    #Turn to drop artifact
-    await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1,300, 300,-300)
-
-    #Drop artifact
-    await motor.run_for_degrees(port.C,200,-400)
-
-
-
-
-
-
-
-
+    #Go back after grabbing artifact
+    await gyro_backward(distance=220,speed=220)
 
 runloop.run(main())
